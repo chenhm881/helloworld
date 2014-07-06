8cc27485feb020601299c9cff68a9e1951d77b06
huiming chen <chm@huimings-MacBook.local>
2014-07-06 08:21:19 +0800

    place seat v2 update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
 
-
-
   def create
     @article = Article.find(params[:comment][:article_id])
     @comment = @article.comments.new(params[:comment])
